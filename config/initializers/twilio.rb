a4dcfd159ddcfaf382dafe139af78823702a5125
Lawrence <lawrencemurry@yahoo.com>
2020-09-05 04:39:30 +0000

    Commented out Twilio test credentials

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,9 +1,9 @@
 Twilio.configure do |config|
 # TEST  
-  config.account_sid = 'AC6203e75dbf9cd35f71f3794343fd6ae8'
-  config.auth_token = 'dfbde9bb12a0a4cbcb522791c0adb035'
+  # config.account_sid = 'AC6203e75dbf9cd35f71f3794343fd6ae8'
+  # config.auth_token = 'dfbde9bb12a0a4cbcb522791c0adb035'
   
 # LIVE  
-  # config.account_sid = ENV["TWILIO_SID"]
-  # config.auth_token = ENV["TWILIO_AUTH"]
+  config.account_sid = ENV["TWILIO_SID"]
+  config.auth_token = ENV["TWILIO_AUTH"]
 end
